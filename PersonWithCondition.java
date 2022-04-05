6ea366f238c5867a2d06d45ab0ceb6ec2fc614d6
Aarti Sahu <101656201+Aarti-itraa@users.noreply.github.com>
2022-04-05 14:09:29 +0530

    Update PersonWithCondition.java

--- PersonWithCondition.java
+++ PersonWithCondition.java
@@ -9,6 +9,8 @@ public class PersonWithCondition {
 		static Consumer<Person> c1 = (per) -> System.out.println(per);
 		static Consumer<Person> c2 = (per) -> 
 		System.out.println(per.getName().toUpperCase());
+		
+		
 		static List<Person> list=PersonRepository.getAllPersons();
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
@@ -17,15 +19,27 @@ public class PersonWithCondition {
 			//		list.forEach(c1);
 			//		list.forEach(c2);
 			//		list.forEach(c1.andThen(c2));
-			printWithCondition();
+			printWithCondition1();
+			printWithCondition2();
+
 		}
 		
-		static void printWithCondition(){
+		static void printWithCondition1(){
+			System.out.println("Person having 2 kids and name starting with 'A'");
 			list.forEach(per ->{
 				if((per.getKids()==2) && (per.getName().startsWith("A")) ){
-					c1.andThen(c2).accept(per);
+					c1.accept(per);
 				}
 			});
+		}
+			static void printWithCondition2(){
+				System.out.println("\nPerson having hobby as Cricket with gender Female");
+				list.forEach(per ->{
+					if((per.getHobbies().contains("Cricket")) && (per.getGender().equals("FEMALE")) ){
+						//c1.andThen(c2).accept(per);
+						c1.accept(per);
+					}
+				});
 	}
 
 }
